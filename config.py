09374400277b61afd8964f045790fd35ebf6de60
Nicolo Castro <nicolo.castro@epress.local>
2017-12-11 17:49:45 +0100

    Starting to add suppot for Cubes, both monodisperse and polydisperse, botha re numerical approximation using scipy integration

--- config.py
+++ config.py
@@ -1,24 +1,33 @@
 _AvlbUnits = ['A','nm','um','mm','cm','m']
 _UnitsSymbols = {'A': '$\AA$','nm':'nm','um':'$\mu$m','mm':'mm','cm':'cm','m':'m'}
 _UnitsConv = {'A':1e-10,'nm':1e-9,'um':1e-6,'mm':1e-3,'cm':1e-2,'m':1e0}
-_AvlbSASFit = ['Sing_Gauss', 'Double_Gauss', 'Sing_Schultz', 'Double_Schultz', 'EM',\
-                'Dist_from_EM']
+_AvlbSASFit = ['Sing_Gauss', 'Double_Gauss', 'Sing_Schultz', 'Double_Schultz',
+                'Mono_Cube', 'Gauss_Cube','EM',\
+                'Dist_from_EM',]
 _AvlbSASFitDic = {'Single Gaussian Distribution' : 'Sing_Gauss',\
                    'Double Gaussian Distribution' : 'Double_Gauss',\
                    'Single Schultz Distribution' : 'Sing_Schultz',\
                    'Double Schultz Distribution' : 'Double_Schultz',\
+                   'Monodisperse Cubes' : 'Mono_Cube',\
+                   'Single Gaussian Cubes' : 'Gauss_Cube',\
                    'Expected Maximization' : 'EM',\
                    'Distribution From Expected Maximization': 'Dist_from_EM'}
 _AvlbSASFitDicInv = {'Sing_Gauss' : 'Single Gaussian Distribution',\
                    'Double_Gauss' : 'Double Gaussian Distribution',\
                    'Sing_Schultz' : 'Single Schultz Distribution',\
                    'Double_Schultz' : 'Double Schultz Distribution',\
+                   'Mono_Cube' : 'Monodisperse Cube',\
+                   'Gauss Cube' : 'Single Gaussian Cubes',\
                    'EM' : 'Expected Maximization',\
                    'Dist_from_EM' : 'Distribution From Expected Maximization'}
 _AvlbWASFitDic = {'Pseudo-Voigt Model': 'PVM'}
 _AvlbWASFit = ['PVM']
-_lmfitModels = ['Sing_Gauss', 'Double_Gauss', 'Sing_Schultz', 'Double_Schultz']
-_lmfitModelFunctions = {'Sing_Gauss': 'single_gauss_spheres', 'Double_Gauss': 'double_gauss_spheres',\
-						'Sing_Schultz': 'single_schultz_spheres', 'Double_Schultz': 'double_schultz_spheres'}
+_lmfitModels = ['Sing_Gauss', 'Double_Gauss', 'Sing_Schultz', 'Double_Schultz', 'Mono_Cube', 'Gauss_Cube']
+_lmfitModelFunctions = {'Sing_Gauss': 'single_gauss_spheres', \
+                        'Double_Gauss': 'double_gauss_spheres',\
+						'Sing_Schultz': 'single_schultz_spheres', \
+                        'Double_Schultz': 'double_schultz_spheres',\
+                        'Mono_Cube' : 'monodisperse_cube',\
+                        'Gauss Cube' : 'single_gaussian_cube',}
 _lmfitDistFunctions = {'Sing_Gauss': 'single_gauss_distribution', 'Double_Gauss': 'double_gauss_distribution',\
                        'Sing_Schultz': 'single_schultz_distribution', 'Double_Schultz': 'double_schultz_distribution'}
